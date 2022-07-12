8d3985461213ec7e74e4e2839d94061d3b1ef978
Victor <victornunesvln2020@gmail.com>
2022-07-12 19:05:10 -0300

    fix: 🛠 map_validate() completed!

--- srcs/map_validate.c
+++ srcs/map_validate.c
@@ -3,52 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   map_validate.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afaustin <afaustin@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: vlima-nu <vlima-nu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/07 19:53:07 by vlima-nu          #+#    #+#             */
-/*   Updated: 2022/07/11 21:40:51 by afaustin         ###   ########.fr       */
+/*   Updated: 2022/07/12 18:42:44 by vlima-nu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "cub3D.h"
 
-static int	validate_horizontal_max_min(char **map, char c, int dir, int index);
+static int	validate_horizontal_max_min(char **map, int dir);
 static void	empty_lines_validate(t_game *game);
 
 void	map_validate(t_game *game)
 {
-	size_t	total_lines;
+	size_t		total_lines;
 	size_t		line;
 
 	empty_lines_validate(game);
 	total_lines = ft_count_vectors((void **)game->params.map);
 	line = 0;
-	if (validate_horizontal_max_min(game->params.map, '0', 0, 0))
-		print_and_exit("Not surrounded by walls 1", game);
-	if (validate_horizontal_max_min(game->params.map + 1, '0', -1, 0))
-		print_and_exit("Not surrounded by walls 2", game);
-	if (validate_horizontal_max_min(game->params.map + total_lines - 1, '0', 1, 0))
-		print_and_exit("Not surrounded by walls 3", game);
-	if (validate_horizontal_max_min(game->params.map + total_lines, '0',0, 0))
-		print_and_exit("Not surrounded by walls 4", game);
-	while (++line < total_lines - 1)
-		if (validate_horizontal_max_min(game->params.map + line, '0', 0, 0))
-			print_and_exit("Not surrounded by walls 5", game);
+	if (validate_horizontal_max_min(game->params.map, 0))
+		error("Not surrounded by walls", game);
+	if (validate_horizontal_max_min(game->params.map + 1, -1))
+		error("Not surrounded by walls", game);
+	if (validate_horizontal_max_min(game->params.map + total_lines - 2, 1))
+		error("Not surrounded by walls", game);
+	if (validate_horizontal_max_min(game->params.map + total_lines - 1, 0))
+		error("Not surrounded by walls", game);
+	while (++line < total_lines - 2)
+		if (validate_horizontal_max_min(game->params.map + line, 0))
+			error("Not surrounded by walls", game);
 }
 
-static int	validate_horizontal_max_min(char **map, char c, int dir, int index)
+static int	validate_horizontal_max_min(char **map, int dir)
 {
 	int		behind;
 	int		limit;
+	int		index;
 
+	index = 0;
 	behind = 0;
 	limit = ft_strlen(map[dir]);
-	printf("%s\n", map[dir]);
 	while (map[0][index])
 	{
-		if (index == 24 && dir)
-			index = 24;
-		if (map[0][index] != c)
+		if (!ft_strchr("0NEWS", map[0][index]))
 		{
 			if (map[0][index] != '1' && behind)
 				return (EXIT_FAILURE);
@@ -83,8 +82,8 @@ static void	empty_lines_validate(t_game *game)
 		if (game->map_cub[i] == '\n')
 		{
 			bl++;
-			if (game->map_cub[i + 1] == '\n' && bl <= lines_qtd)
-				print_and_exit("Invalid empty line on map", game);
+			if (game->map_cub[i + 1] == '\n' && bl < lines_qtd)
+				error("Invalid empty line on map", game);
 		}
 	}
 }
