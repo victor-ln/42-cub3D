993ce05a273ac56715d185f97d65e4eb22dbe2df
Adriano Faustino <faustino.adr@gmail.com>
2022-07-20 22:32:10 -0300

    feat: :rocket: All events are working and added put image to window after drawing it

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vlima-nu <vlima-nu@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: afaustin <afaustin@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 16:39:55 by adrianofaus       #+#    #+#             */
-/*   Updated: 2022/07/19 22:55:45 by vlima-nu         ###   ########.fr       */
+/*   Updated: 2022/07/20 22:17:38 by afaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static void	init_game(t_game *game);
 static void	start_game(t_game *game);
-static void	display_game(t_game *g);
+static int	display_game(t_game *g);
 
 int	main(int argc, char **argv)
 {
@@ -25,7 +25,8 @@ int	main(int argc, char **argv)
 	load_file(game, argc, argv[argc - 1]);
 	save_params(game);
 	file_validate(game);
-	load_environment(game);
+	print_matrix(game->params.map);
+	load_game(game);
 	start_game(game);
 	end_program(game);
 	return (0);
@@ -37,6 +38,7 @@ static void	init_game(t_game *game)
 		error("Malloc Failed", game);
 	ft_bzero(game, sizeof(t_game));
 	ft_bzero(&game->params, sizeof(t_params));
+	ft_bzero(&game->player, sizeof(t_player));
 	game->params.textures = ft_calloc(sizeof(char *), 4);
 	if (!game->params.textures)
 		error("Malloc Failed", game);
@@ -45,16 +47,18 @@ static void	init_game(t_game *game)
 static void	start_game(t_game *game)
 {
 	display_game(game);
-	mlx_hook(game->window, 17, 0L, close_window, game);
+	mlx_hook(game->window, 2, 1, key_press, game);
+	mlx_hook(game->window, 3, 1 << 1, key_release, game);
+	mlx_hook(game->window, 17, 0, close_window, game);
 	mlx_expose_hook(game->window, reload_image, game);
-	// mlx_key_hook(game->window, key_press, game);
-	// mlx_loop_hook(game->mlx, animation, game);
+	mlx_loop_hook(game->mlx, display_game, game);
 	mlx_loop(game->mlx);
 }
 
-static void	display_game(t_game *g)
+static int	display_game(t_game *g)
 {
-	// draw_game(g);
+	draw_game(g);
 	mlx_do_sync(g->mlx);
 	mlx_put_image_to_window(g->mlx, g->window, g->img, 0, 0);
+	return (0);
 }
