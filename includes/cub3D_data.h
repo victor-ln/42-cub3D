cc5f9f5ea5773df61d9dbdf173225a63899dc79a
vlima-nu <vlima-nu@student.42sp.org.br>
2022-08-17 18:21:12 -0300

    feat: :rocket: renaming xpms and adding path in header file

--- includes/cub3D_data.h
+++ includes/cub3D_data.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D_data.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afaustin <afaustin@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: vlima-nu <vlima-nu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 20:59:56 by vlima-nu          #+#    #+#             */
-/*   Updated: 2022/08/16 21:00:01 by afaustin         ###   ########.fr       */
+/*   Updated: 2022/08/17 18:19:16 by vlima-nu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,6 +86,93 @@
 # define DARK_FRONT_DOOR		"./assets/xpm/walls/iron_door_02.xpm"
 # define LIGHT_SIDE_DOOR		"./assets/xpm/walls/iron_wall_02.xpm"
 # define DARK_SIDE_DOOR			"./assets/xpm/walls/iron_wall_03.xpm"
+
+# define OBJECT_00				"./assets/xpm/objects/ammo.xpm"
+# define OBJECT_01				"./assets/xpm/objects/armor.xpm"
+# define OBJECT_02				"./assets/xpm/objects/barrel.xpm"
+# define OBJECT_03				"./assets/xpm/objects/bed.xpm"
+# define OBJECT_04				"./assets/xpm/objects/blood.xpm"
+# define OBJECT_05				"./assets/xpm/objects/blue_key.xpm"
+# define OBJECT_06				"./assets/xpm/objects/brass.xpm"
+# define OBJECT_07				"./assets/xpm/objects/bucket.xpm"
+# define OBJECT_08				"./assets/xpm/objects/bunch_skeletons.xpm"
+# define OBJECT_09				"./assets/xpm/objects/cage.xpm"
+# define OBJECT_10				"./assets/xpm/objects/cage_skeleton.xpm"
+# define OBJECT_11				"./assets/xpm/objects/crown.xpm"
+# define OBJECT_12				"./assets/xpm/objects/crucifix.xpm"
+# define OBJECT_13				"./assets/xpm/objects/cup.xpm"
+# define OBJECT_14				"./assets/xpm/objects/drinking_fountain.xpm"
+# define OBJECT_15				"./assets/xpm/objects/empty_pit.xpm"
+# define OBJECT_16				"./assets/xpm/objects/flag.xpm"
+# define OBJECT_17				"./assets/xpm/objects/food_01.xpm"
+# define OBJECT_18				"./assets/xpm/objects/food_02.xpm"
+# define OBJECT_19				"./assets/xpm/objects/key.xpm"
+# define OBJECT_20				"./assets/xpm/objects/lamp_01.xpm"
+# define OBJECT_21				"./assets/xpm/objects/lamp_02.xpm"
+# define OBJECT_22				"./assets/xpm/objects/lamp_03.xpm"
+# define OBJECT_23				"./assets/xpm/objects/little_table.xpm"
+# define OBJECT_24				"./assets/xpm/objects/machine_gun.xpm"
+# define OBJECT_25				"./assets/xpm/objects/medkit.xpm"
+# define OBJECT_26				"./assets/xpm/objects/oven.xpm"
+# define OBJECT_27				"./assets/xpm/objects/pillar.xpm"
+# define OBJECT_28				"./assets/xpm/objects/pitcher.xpm"
+# define OBJECT_29				"./assets/xpm/objects/pit.xpm"
+# define OBJECT_30				"./assets/xpm/objects/plant_01.xpm"
+# define OBJECT_31				"./assets/xpm/objects/plant_02.xpm"
+# define OBJECT_32				"./assets/xpm/objects/portrait.xpm"
+# define OBJECT_33				"./assets/xpm/objects/pots_01.xpm"
+# define OBJECT_34				"./assets/xpm/objects/pots_02.xpm"
+# define OBJECT_35				"./assets/xpm/objects/puddle.xpm"
+# define OBJECT_36				"./assets/xpm/objects/roots.xpm"
+# define OBJECT_37				"./assets/xpm/objects/skeleton_blood.xpm"
+# define OBJECT_38				"./assets/xpm/objects/skeleton.xpm"
+# define OBJECT_39				"./assets/xpm/objects/skeleton_lying.xpm"
+# define OBJECT_40				"./assets/xpm/objects/spears.xpm"
+# define OBJECT_41				"./assets/xpm/objects/submachine_gun.xpm"
+# define OBJECT_42				"./assets/xpm/objects/table_chairs.xpm"
+# define OBJECT_43				"./assets/xpm/objects/trash_01.xpm"
+# define OBJECT_44				"./assets/xpm/objects/trash_02.xpm"
+# define OBJECT_45				"./assets/xpm/objects/trash_03.xpm"
+# define OBJECT_46				"./assets/xpm/objects/treasure.xpm"
+
+# define KNIFE_00				"./assets/xpm/weapons/knife_01.xpm"
+# define KNIFE_01				"./assets/xpm/weapons/knife_02.xpm"
+# define KNIFE_02				"./assets/xpm/weapons/knife_03.xpm"
+# define KNIFE_03				"./assets/xpm/weapons/knife_04.xpm"
+# define KNIFE_04				"./assets/xpm/weapons/knife_05.xpm"
+
+# define PISTOL_00				"./assets/xpm/weapons/pistol_01.xpm"
+# define PISTOL_01				"./assets/xpm/weapons/pistol_02.xpm"
+# define PISTOL_02				"./assets/xpm/weapons/pistol_03.xpm"
+# define PISTOL_03				"./assets/xpm/weapons/pistol_04.xpm"
+# define PISTOL_04				"./assets/xpm/weapons/pistol_05.xpm"
+
+# define MACHINE_GUN_00				"./assets/xpm/weapons/machine_gun_01.xpm"
+# define MACHINE_GUN_01				"./assets/xpm/weapons/machine_gun_02.xpm"
+# define MACHINE_GUN_02				"./assets/xpm/weapons/machine_gun_03.xpm"
+# define MACHINE_GUN_03				"./assets/xpm/weapons/machine_gun_04.xpm"
+# define MACHINE_GUN_04				"./assets/xpm/weapons/machine_gun_05.xpm"
+
+# define SUBMACHINE_GUN_00				"./assets/xpm/weapons/submachine_gun_01.xpm"
+# define SUBMACHINE_GUN_01				"./assets/xpm/weapons/submachine_gun_02.xpm"
+# define SUBMACHINE_GUN_02				"./assets/xpm/weapons/submachine_gun_03.xpm"
+# define SUBMACHINE_GUN_03				"./assets/xpm/weapons/submachine_gun_04.xpm"
+# define SUBMACHINE_GUN_04				"./assets/xpm/weapons/submachine_gun_05.xpm"
+
+# define GUARD_00						"./assets/xpm/guard/front_idle_guard_01.xpm"
+# define GUARD_01						"./assets/xpm/guard/backwards_idle_guard_01.xpm"
+# define GUARD_02						"./assets/xpm/guard/shooting_guard_01.xpm"
+# define GUARD_03						"./assets/xpm/guard/shooting_guard_02.xpm"
+# define GUARD_04						"./assets/xpm/guard/dying_guard_01.xpm"
+# define GUARD_05						"./assets/xpm/guard/dying_guard_02.xpm"
+# define GUARD_06						"./assets/xpm/guard/dying_guard_03.xpm"
+# define GUARD_07						"./assets/xpm/guard/dying_guard_04.xpm"
+# define GUARD_08						"./assets/xpm/guard/dying_guard_05.xpm"
+
+# define WEAPONS_TYPES			4
+# define WEAPONS_FRAMES			5
+# define OBJECTS_NUM			47
+# define GUARD_NUM				9
 
 typedef enum e_minimap_size{
 	BIG,
@@ -212,6 +299,9 @@ typedef struct s_game
 	t_img					*crosshair[2];
 	t_img					*wall_textures[TEXTURES_NUM];
 	t_img					*door_textures[TEXTURES_NUM];
+	t_img					*weapons[WEAPONS_TYPES][WEAPONS_FRAMES];
+	t_img					*enemies[GUARD_NUM];
+	t_img					*objects[OBJECTS_NUM];
 	t_texture_properties	*texture_prop;
 	t_wall_properties		*wall_prop;
 	t_rays_properties		*ray_prop;
